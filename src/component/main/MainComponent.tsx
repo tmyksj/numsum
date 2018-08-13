f5e71a55283bcf37b9f196039c79a0fcc740cd0f
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-08-14 08:23:23 +0900

    update description

--- src/component/main/MainComponent.tsx
+++ src/component/main/MainComponent.tsx
@@ -10,7 +10,7 @@ class MainComponent extends React.Component {
             <main className="MainComponent-main">
                 <p className="MainComponent-main__description">
                     The number of the cell is added around the cell.<br/>
-                    If all becomes 0, the stage is clear.
+                    Your goal is to make all cells 0.
                 </p>
                 <StageComponent/>
             </main>
